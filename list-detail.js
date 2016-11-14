d5495d83062961278f1a76b55d22b08c49311440
alirni <aliramezani19.1@gmail.com>
2016-11-14 15:46:25 +0330

    feat(list-detail.js): PE-36 update list-detail.js

--- list-detail.js
+++ list-detail.js
@@ -9,13 +9,18 @@
 				properties: {
 
 					/**
+					 * Variable for get name shop.
+					 */
+					shopname : {
+						type: String,
+						value: 'Big Shop' //Temporary
+					},
+
+					/**
 					* The z-depth of this element, from 0-5. Setting to 0 will remove the
 					* shadow, and each increasing number greater than 0 will be "deeper"
 					* than the last.
 					*
-					* @attribute elevation
-					* @type number
-					* @default 1
 					*/
 					elevation: {
 						type: Number,
@@ -23,26 +28,7 @@
 						value: 1
 					},
 
-				},
-
-				/**
-				 * reset panel-filter.
-				 */
-				_resetfilter : function () {
-					this.search = "";
-					this.numbersearch = "";
-
-					var select = this.querySelectorAll('.checkchange');
-					for (var i = 0; i<=5; i++) {
-						select[i].checked = false;
-					}
-
-					var dropselect = this.$$('#dropdown');
-					// console.log("1" , dropselect);
-					dropselect.selectedItem  = null;
-					// console.log("2" , dropselect);
-				},
-
+				}
 			}
 		}
 	}
