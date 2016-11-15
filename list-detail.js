1c5d399e82116fe9106ee746a22d756a3a472871
alirni <aliramezani19.1@gmail.com>
2016-11-15 08:29:09 +0330

    fix: PE-32 tap

--- list-detail.js
+++ list-detail.js
@@ -1,36 +1,36 @@
 (function (){
-	'use strict';
+  'use strict';
 
-	class ListDetail extends Polymer.Element {
-		static get is () { return 'list-detail'; }
+  class ListDetail extends Polymer.Element {
+    static get is () { return 'list-detail'; }
 
-		static get config () {
-			return {
-				properties: {
+    static get config () {
+      return {
+        properties: {
 
-					/**
-					 * Variable for get name shop.
-					 */
-					shopname: {
-						type: String,
-						value: 'Big Shop' //Temporary
-					},
+          /**
+           * Variable for get name shop.
+           */
+          shopname: {
+            type: String,
+            value: 'Big Shop' //Temporary
+          },
 
-					/**
-					* The z-depth of this element, from 0-5. Setting to 0 will remove the
-					* shadow, and each increasing number greater than 0 will be "deeper"
-					* than the last.
-					*/
-					elevation: {
-						type: Number,
-						reflectToAttribute: true,
-						value: 1
-					},
+          /**
+          * The z-depth of this element, from 0-5. Setting to 0 will remove the
+          * shadow, and each increasing number greater than 0 will be "deeper"
+          * than the last.
+          */
+          elevation: {
+            type: Number,
+            reflectToAttribute: true,
+            value: 1
+          },
 
-				}
-			};
-		}
-	}
+        }
+      };
+    }
+  }
 
-	customElements.define(ListDetail.is, ListDetail);
+  customElements.define(ListDetail.is, ListDetail);
 })();
