61d55dfd92b54329e84310f35232ddebe2a0fb3a
alirni <aliramezani19.1@gmail.com>
2016-11-27 10:19:33 +0330

    feat(page-entity-detail): PE-41 add elevation properti

--- page-entity-detail.js
+++ page-entity-detail.js
@@ -11,7 +11,19 @@
           //for get id
           route : {
             type : Object,
-          }
+          },
+
+          /**
+          * The z-depth of this element, from 0-5. Setting to 0 will remove the
+          * shadow, and each increasing number greater than 0 will be "deeper"
+          * than the last.
+          */
+          elevation: {
+            type: Number,
+            reflectToAttribute: true,
+            value: 1
+          },
+
         }
       };
     } // end config
