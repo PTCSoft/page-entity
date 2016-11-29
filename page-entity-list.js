38220d6a57736916cedf9dc8519b13f658eb121d
alirni <aliramezani19.1@gmail.com>
2016-11-29 09:33:21 +0330

    feat(page-entity-list.js): PE-41 use on-tap

--- page-entity-list.js
+++ page-entity-list.js
@@ -1,16 +1,30 @@
 (function (){
-	'use strict';
+  'use strict';
 
-	class PageEntityList extends Polymer.Element {
-		static get is () { return 'page-entity-list'; }
+  class PageEntityList extends Polymer.Element {
+    static get is () { return 'page-entity-list'; }
 
-		static get config () {
-			return {
-				properties: {
-				}
-			};
-		} // end config
-	}
+    static get config () {
+      return {
+        properties: {
+          dataindex: {
+            type: Number
+          }
+        }
+      };
+    } // end config
 
-	customElements.define(PageEntityList.is, PageEntityList);
+    tap (event) {
+      // console.log('************ TAP **************');
+
+      this.dataindex = event.model.item.index;
+      // console.log('dataindex(ID) : ' + this.dataindex);
+
+      this.fire('sendindex', this.dataindex);
+
+      // console.log('************ TAP **************');
+    }
+  }
+
+  customElements.define(PageEntityList.is, PageEntityList);
 })();
