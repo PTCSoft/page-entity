e6d143c29aac55dcd5ad976e2777aaf902e8971e
alirni <aliramezani19.1@gmail.com>
2016-11-29 09:57:43 +0330

    feat(page-entity): PE-41 add send function

--- page-entity.js
+++ page-entity.js
@@ -1,16 +1,30 @@
 (function (){
-	'use strict';
+  'use strict';
 
-	class PageEntity extends Polymer.Element {
-		static get is () { return 'page-entity'; }
+  class PageEntity extends Polymer.Element {
+    static get is () { return 'page-entity'; }
 
-		static get config () {
-			return {
-				properties: {
-				}
-			};
-		} // end config
-	}
+    static get config () {
+      return {
+        properties: {
+        }
+      };
+    } // end config
 
-	customElements.define(PageEntity.is, PageEntity);
+     send (event) {
+      console.log('************ SEND *************');
+
+      // console.log('routeData ' + this.routeData.page);
+      // console.log('route.path ' + this.route.path);
+
+      this.set('route.path', route.path + event.detail);
+
+      // console.log('route.path ' + this.route.path);
+      // console.log('subroute ' + this.subroute.path);
+
+      // console.log('************ SEND *************');
+    }
+  }
+
+  customElements.define(PageEntity.is, PageEntity);
 })();
